30c85e1323bf20c0b2431e3147f3299b8202e301
mcsewf-gif <mcsewf@gmail.com>
2026-08-23 16:47:34 -0400

    Add service worker for PWA caching
    Implement service worker for caching resources.

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'mw-pwa-v2';
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
